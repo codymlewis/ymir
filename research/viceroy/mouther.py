37e42991bdf5081e9071b011969acca92ed21cc4
Cody Lewis <cody@codymlewis.com>
2021-10-12 15:43:58 +1100

    Updated mouther research code

--- research/viceroy/mouther.py
+++ research/viceroy/mouther.py
@@ -1,68 +1,17 @@
-from jax._src import util
-from endpoints.adversaries import AdvController, MoutherController, OnOffController, OnOffFRController, ScalingController, FRController
 from functools import partial
-import pickle
 import sys
+import pickle
+import pandas as pd
 
 import jax
-import jax.flatten_util
 import jax.numpy as jnp
+import jax.flatten_util
 import optax
 import haiku as hk
 
-from tqdm import trange, tqdm
-import pandas as pd
+from tqdm import trange
 
-import chief
-from chief.aggregation import foolsgold, std_dagmm
-from chief.aggregation import fed_avg, krum, viceroy
-import endpoints
-import utils
-
-def update_and_scale(alg, server, all_grads, round):
-    if alg == 'fed_avg':
-        alpha = fed_avg.scale(server.batch_sizes)
-    elif alg == 'foolsgold':
-        server.histories = foolsgold.update(server.histories, all_grads)
-        alpha = foolsgold.scale(server.histories, server.kappa)
-    elif alg == 'krum':
-        alpha = krum.scale(all_grads, 3)
-    elif alg == 'std_dagmm':
-        server.params, server.opt_state = std_dagmm.update(server, all_grads)
-        alpha = std_dagmm.scale(server.batch_sizes, all_grads, server)
-    else:
-        alpha = viceroy.scale(all_grads, server.histories, server.reps, round + 1)
-        if round > 0:
-            server.histories, server.reps = viceroy.update(server.histories, server.reps, all_grads)
-        else:
-            server.histories = viceroy.init(all_grads)
-    return alpha, server
-
-def update(alg, server, all_grads, round):
-    if alg == 'foolsgold':
-        server.histories = foolsgold.update(server.histories, all_grads)
-    elif alg == 'std_dagmm':
-        server.params, server.opt_state = std_dagmm.update(server, all_grads)
-    elif alg == 'viceroy':
-        if round > 0:
-            server.histories, server.reps = viceroy.update(server.histories, server.reps, all_grads)
-        else:
-            server.histories = viceroy.init(all_grads)
-    return server
-
-def scale(alg, server, all_grads, round):
-    if alg == 'fed_avg':
-        alpha = fed_avg.scale(server.batch_sizes)
-    elif alg == 'foolsgold':
-        alpha = foolsgold.scale(server.histories, server.kappa)
-    elif alg == 'krum':
-        alpha = krum.scale(all_grads, 3)
-    elif alg == 'std_dagmm':
-        alpha = std_dagmm.scale(server.batch_sizes, all_grads, server)
-    else:
-        alpha = viceroy.scale(all_grads, server.histories, server.reps, round + 1)
-    return alpha
-
+import ymir
 
 @jax.jit
 def euclid_dist(a, b):
@@ -73,102 +22,92 @@ def unzero(x):
 
 
 if __name__ == "__main__":
-    print("Starting up...")
     adv_percent = [0.1, 0.3, 0.5, 0.8]
-    mouth_results = pd.DataFrame(columns=["algorithm", "attack", "dataset"] + [f"{p} mean asr" for p in adv_percent] + [f"{p} std asr" for p in adv_percent])
+    onoff_results = pd.DataFrame(columns=["algorithm", "attack", "dataset"] + [f"{p} mean asr" for p in adv_percent] + [f"{p} std asr" for p in adv_percent])
+    print("Starting up...")
     IID = False
-    for DATASET in [utils.datasets.MNIST, utils.datasets.KDDCup99, utils.datasets.CIFAR10]:
+    VICTIM = 0
+    for DATASET in [ymir.mp.datasets.KDDCup99]:
         DATASET = DATASET()
-        for ALG in ["viceroy"]:
-            IID = False
-            if type(DATASET).__name__ == 'KDDCup99':
-                T = 20
-            else:
-                T = 10
-            VICTIM = 0
-            ADV = "bad mouther"
-            ADV_CLASS = endpoints.Client
-            SERVER_CLASS = {
-                'fed_avg': lambda: fed_avg.Server(jnp.array([c.batch_size for c in clients])),
-                'foolsgold': lambda: foolsgold.Server(len(clients), params, 1.0),
-                'krum': lambda: None,
-                'viceroy': lambda: viceroy.Server(len(clients), params),
-                'std_dagmm': lambda: std_dagmm.Server(jnp.array([c.batch_size for c in clients]), params)
-            }[ALG]
-            cur = {"algorithm": ALG, "attack": ADV, "dataset": type(DATASET).__name__}
-            for acal in adv_percent:
-                print(f"Running {ALG} on {type(DATASET).__name__}{'-iid' if IID else ''} with {acal:.0%} {ADV} adversaries")
-                net = hk.without_apply_rng(hk.transform(lambda x: utils.nets.LeNet(DATASET.classes)(x)))
-                opt = optax.sgd(0.01)
-                server_update = chief.update(opt)
-                client_update = endpoints.update(opt, utils.losses.cross_entropy_loss(net, DATASET.classes))
-                evaluator = utils.metrics.measurer(net)
+        for ALG in ["foolsgold"]:
+            for ADV in ["bad mouther"]:
+                if type(DATASET).__name__ == 'KDDCup99':
+                    T = 20
+                else:
+                    T = 10
+                cur = {"algorithm": ALG, "attack": ADV, "dataset": type(DATASET).__name__}
+                for acal in adv_percent:
+                    print(f"Running {ALG} on {type(DATASET).__name__}{'-iid' if IID else ''} with {acal:.0%} {ADV} adversaries")
+                    if type(DATASET).__name__ == 'CIFAR10':
+                        net = hk.without_apply_rng(hk.transform(lambda x: ymir.mp.models.ConvLeNet(DATASET.classes)(x)))
+                    else:
+                        net = hk.without_apply_rng(hk.transform(lambda x: ymir.mp.models.LeNet(DATASET.classes)(x)))
 
-                A = int(T * acal)
-                N = T - A
-                batch_sizes = [8 for _ in range(N + A)]
-                data = DATASET.fed_split(batch_sizes, IID)
+                    train_eval = DATASET.get_iter("train", 10_000)
+                    test_eval = DATASET.get_iter("test")
+                    opt = optax.sgd(0.01)
+                    params = net.init(jax.random.PRNGKey(42), next(test_eval)[0])
+                    opt_state = opt.init(params)
+                    loss = ymir.mp.losses.cross_entropy_loss(net, DATASET.classes)
 
-                train_eval = DATASET.get_iter("train", 10_000)
-                test_eval = DATASET.get_iter("test")
+                    A = int(T * acal)
+                    N = T - A
+                    batch_sizes = [8 for _ in range(N + A)]
+                    data = DATASET.fed_split(batch_sizes, IID)
 
-                params = net.init(jax.random.PRNGKey(42), next(test_eval)[0])
-                opt_state = opt.init(params)
+                    network = ymir.mp.network.Network(opt, loss)
+                    network.add_controller(
+                        "main",
+                        con_class = partial(ymir.scout.adversaries.MoutherController, num_adversaries=A, victim=VICTIM, attack_type=ADV),
+                        is_server=True
+                    )
 
-                clients = [endpoints.Client(opt_state, data[i],  batch_sizes[i]) for i in range(N)]
-                clients += [ADV_CLASS(opt_state, data[i + N], batch_sizes[i + N]) for i in range(A)]
-                server = SERVER_CLASS()
+                    for i in range(T):
+                        network.add_host("main", ymir.scout.Client(opt_state, data[i]))
+                    controller = network.get_controller("main")
+                    if "onoff" in ADV:
+                        controller.init()
 
-                results = utils.metrics.create_recorder(['accuracy'], train=True, test=True, add_evals=['attacking'])
-                results['asr'] = []
+                    evaluator = ymir.mp.metrics.measurer(net)
 
-                controller = MoutherController(A, VICTIM, ADV)
+                    model = ymir.Coordinate(ALG, opt, opt_state, params, network)
 
-                # Train/eval loop.
-                TOTAL_ROUNDS = 5_001
-                pbar = trange(TOTAL_ROUNDS)
-                for round in pbar:
-                    # Client side training
-                    all_grads = []
-                    for client in clients:
-                        grads, client.opt_state = client_update(params, client.opt_state, *next(client.data))
-                        all_grads.append(grads)
+                    results = ymir.mp.metrics.create_recorder(['accuracy'], train=True, test=True, add_evals=['attacking'])
+                    results['asr'] = []
 
-                    # Adversary interception and decision
-                    if A > 0:
-                        controller.intercept(all_grads)
+                    # Train/eval loop.
+                    TOTAL_ROUNDS = 5_001
+                    pbar = trange(TOTAL_ROUNDS)
+                    for round in pbar:
+                        alpha, all_grads = model.fit()
 
-                    # Server side collection of gradients
-                    server = update(ALG, server, all_grads, round)
-                    alpha = scale(ALG, server, all_grads, round)
-
-                    all_grads = chief.apply_scale(alpha, all_grads)
-
-                    if round % 10 == 0:
-                        if (alpha[-A:] < 0.0001).all():
-                            asr = -1 if alpha[VICTIM] < 0.0001 else -2
-                        else:
-                            theta = jax.flatten_util.ravel_pytree(chief.sum_grads(all_grads))[0]
-                            vicdel = euclid_dist(jax.flatten_util.ravel_pytree(all_grads[VICTIM])[0], theta)
-                            if "good" in ADV:
-                                numerator = min(euclid_dist(jnp.array([jax.flatten_util.ravel_pytree(g)[0] for g in all_grads]), theta))
-                                asr = unzero(numerator) / unzero(vicdel)
+                        if round % 10 == 0:
+                            if (alpha[-A:] < 0.0001).all():
+                                asr = -1 if alpha[VICTIM] < 0.0001 else -2
                             else:
-                                asr = unzero(vicdel) / unzero(max(euclid_dist(jnp.array([jax.flatten_util.ravel_pytree(g)[0] for g in all_grads]), theta)))
-                        results['asr'].append(asr)
-                        utils.metrics.record(results, evaluator, params, train_eval, test_eval, {'attacking': controller.attacking})
-                        pbar.set_postfix({'ACC': f"{results['test accuracy'][-1]:.3f}", 'ASR': f"{results['asr'][-1]:.3f}", 'ATT': controller.attacking})
+                                theta = jax.flatten_util.ravel_pytree(ymir.garrison.sum_grads(all_grads))[0]
+                                vicdel = euclid_dist(jax.flatten_util.ravel_pytree(all_grads[VICTIM])[0], theta)
+                                if "good" in ADV:
+                                    numerator = min(euclid_dist(jnp.array([jax.flatten_util.ravel_pytree(g)[0] for g in all_grads]), theta))
+                                    asr = unzero(numerator) / unzero(vicdel)
+                                else:
+                                    asr = unzero(vicdel) / unzero(max(euclid_dist(jnp.array([jax.flatten_util.ravel_pytree(g)[0] for g in all_grads]), theta)))
+                            results['asr'].append(asr)
+                            ymir.mp.metrics.record(results, evaluator, params, train_eval, test_eval, {'attacking': controller.attacking})
+                            pbar.set_postfix({'ACC': f"{results['test accuracy'][-1]:.3f}", 'ASR': f"{results['asr'][-1]:.3f}", 'ATT': controller.attacking})
 
-                    # Server side aggregation
-                    params, opt_state = server_update(params, opt_state, chief.sum_grads(all_grads))
-                results = utils.metrics.finalize(results)
-                cur[f"{acal} mean asr"] = results['asr'].mean()
-                cur[f"{acal} std asr"] = results['asr'].std()
-                print()
-                print("=" * 150)
-                print(f"Server type {ALG}, Dataset {type(DATASET).__name__}, {A / (A + N):.2%} {ADV} adversaries, final accuracy: {results['test accuracy'][-1]:.3%}")
-                print(utils.metrics.tabulate(results, TOTAL_ROUNDS))
-                print("=" * 150)
-                print()
-            mouth_results = mouth_results.append(cur, ignore_index=True)
-    print(mouth_results.to_latex())
+                    results = ymir.mp.metrics.finalize(results)
+                    cur[f"{acal} mean asr"] = results['test asr'].mean()
+                    cur[f"{acal} std asr"] = results['test asr'].std()
+                    print()
+                    print("=" * 150)
+                    print(f"Server type {ALG}, Dataset {type(DATASET).__name__}, {A / (A + N):.2%} {ADV} adversaries, final accuracy: {results['test accuracy'][-1]:.3%}")
+                    print(ymir.mp.metrics.tabulate(results, TOTAL_ROUNDS))
+                    print("=" * 150)
+                    print()
+                onoff_results = onoff_results.append(cur, ignore_index=True)
+    print(onoff_results.to_latex())
+    fn = "results.pkl"
+    with open(fn, 'wb') as f:
+        pickle.dump(results, f)
+    print(f"Written results to {fn}")
