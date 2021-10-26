abfae5dcb5bbcd15bbd9eff7c83b664ecf346642
Cody Lewis <cody@codymlewis.com>
2021-10-27 10:15:40 +1100

    version update

--- ymirlib/setup.py
+++ ymirlib/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ymirlib",
-    version="0.0.1",
+    version="0.0.2",
     author="Cody Lewis",
     description="JAX-based Federated Learning utility library",
     long_description=LONG_DESCRIPTION,
