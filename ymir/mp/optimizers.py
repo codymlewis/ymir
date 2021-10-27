d8315f0d1a1859577807a49bc52a7e58bc751481
Cody Lewis <cody@codymlewis.com>
2021-10-27 15:47:36 +1100

    hide pgd regularizer term

--- ymir/mp/optimizers.py
+++ ymir/mp/optimizers.py
@@ -14,24 +14,23 @@ def pgd(learning_rate, mu):
     Perturbed gradient descent proposed as the mechanism for FedProx in https://arxiv.org/abs/1812.06127
     """
     return optax.chain(
-        add_reg(mu),
+        _add_reg(mu),
         optax.scale(learning_rate)
     )
 
 
-def add_reg(
-    mu: float
-) -> optax.GradientTransformation:
-  """
-  """
+def _add_reg(mu: float) -> optax.GradientTransformation:
+    """
+    Adds a regularization term to the gradient.
+    """
 
-  def init_fn(_):
-    return optax.EmptyState()
+    def init_fn(_):
+        return optax.EmptyState()
 
-  def update_fn(updates, state, params):
-    if params is None:
-      raise ValueError("params argument required for this transform")
-    updates = jax.tree_multimap(lambda g, p: g + mu * ((p - g) - p), updates, params)
-    return updates, state
+    def update_fn(updates, state, params):
+        if params is None:
+            raise ValueError("params argument required for this transform")
+        updates = jax.tree_multimap(lambda g, p: g + mu * ((p - g) - p), updates, params)
+        return updates, state
 
-  return optax.GradientTransformation(init_fn, update_fn)
+    return optax.GradientTransformation(init_fn, update_fn)
