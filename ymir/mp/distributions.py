fe0b6f66f008babc7e00cfe4b4713d76f9d4416d
Cody Lewis <cody@codymlewis.com>
2022-02-07 16:52:11 +1100

    Improved logging usage

--- ymir/mp/distributions.py
+++ ymir/mp/distributions.py
@@ -16,6 +16,8 @@ import logging
 
 import numpy as np
 
+logger = logging.getLogger(__name__)
+
 
 def homogeneous(X, y, nendpoints, nclasses, rng):
     """Assign all data to all endpoints"""
@@ -43,7 +45,7 @@ def lda(X, y, nendpoints, nclasses, rng, alpha=0.5):
         rng.shuffle(idx_c)
         dists_c = np.split(idx_c, np.round(np.cumsum(proportions[c]) * len(idx_c)).astype(int)[:-1])
         distribution = [distribution[i] + d.tolist() for i, d in enumerate(dists_c)]
-    logging.debug(f"distribution: {proportions}")
+    logger.info(f"distribution:\n{np.array_str(proportions, precision=4, suppress_small=True)}")
     return distribution
 
 
